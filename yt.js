d7cb854502701d48085f1b5a7eaf7013c6fea988
poongkabilan229 <poongkabilan.r@codingmart.com>
2019-12-24 17:15:11 +0530

    commit search1

--- yt.js
+++ yt.js
@@ -1,8 +1,7 @@
 function drop() {
     document.getElementById("myDropdown").classList.toggle("show");
+    
   }
-  
-  // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.oppost')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -16,24 +15,27 @@ function drop() {
     }
    
   }
+  // Close the dropdown if the user clicks outside of it
+ 
 
   function drop1() {
     document.getElementById("myDropdown1").classList.toggle("show1");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.opapps')) {
-      var dropdowns1 = document.getElementsByClassName("dropdown-content1");
-      var i;
-      for (i = 0; i < dropdowns1.length; i++) {
-        var openDropdown1 = dropdowns1[i];
-        if (openDropdown1.classList.contains('show1')) {
-          openDropdown1.classList.remove('show1');
+    window.onclick = function(event) {
+      if (!event.target.matches('.opapps')) {
+        var dropdowns1 = document.getElementsByClassName("dropdown-content1");
+        var i;
+        for (i = 0; i < dropdowns1.length; i++) {
+          var openDropdown1 = dropdowns1[i];
+          if (openDropdown1.classList.contains('show1')) {
+            openDropdown1.classList.remove('show1');
+          }
         }
       }
     }
   }
+  
+  // Close the dropdown if the user clicks outside of it
+  
  
 function music(){
     document.getElementById("startid").style.display="none";
@@ -77,4 +79,39 @@ function sports(){
 // }
 // var str = "Visit W3Schools!";
 // var n = str.search("W3Schools");
-
+// function menu(){
+//   var click=document.getElementById("sidediv");
+//   if(click.left="0%")
+//   {
+//   document.getElementById("sidediv").style.left="-17%";
+//   document.getElementById("content").style.marginLeft="5%";
+//   document.getElementById("content").style.width="100%";
+//   }
+//   else 
+//   {
+//     document.getElementById("sidediv").style.left="0%";
+//     document.getElementById("content").style.marginLeft="18%";
+//     document.getElementById("content").style.width="100%";
+//   }
+// }
+function menu(){
+  var x=document.getElementById("para");
+  var y=document.getElementById("cata");
+  if((x.style.display==="block")&&(y.style.display==="block")){
+  document.getElementById("para").style.display="none";
+  document.getElementById("cata").style.display="none";
+  document.getElementById("sidediv").style.width="4%";
+  document.getElementById("sidediv").style.overflow="hidden";
+  document.getElementById("content").style.marginLeft="5%";
+  document.getElementById("content").style.width="100%";
+  }
+  else
+  {
+    document.getElementById("para").style.display="block";
+  document.getElementById("cata").style.display="block";
+  document.getElementById("sidediv").style.width="16%";
+  document.getElementById("sidediv").style.overflow="scroll";
+  document.getElementById("content").style.marginLeft="18%";
+    document.getElementById("content").style.width="100%";
+  }
+}
